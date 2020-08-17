18c6a59922329b376bfbc49f9a0a23485fdf28a0
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-08-17 12:28:24 -0400

    got result cards hooked up to the right classes

--- app.js
+++ app.js
@@ -20,27 +20,52 @@ const listRoutes = (routeData) => {
     const routeSection = document.querySelector(".results-section")
 
     const resultCard = document.createElement("div")
-    resultCard.classList.add(".result-card")
+    resultCard.classList.add("result-card")
     routeSection.append(resultCard)
+
+    const resultCardRow1 = document.createElement("div")
+    resultCardRow1.classList.add("result-row1")
+    resultCard.append(resultCardRow1)
+
+    const resultRow1Title = document.createElement("div")
+    resultRow1Title.classList.add("result-row1-title")
+    resultCardRow1.append(resultRow1Title)
 
     const routeTitle = document.createElement("h4")
     const routeTitleName = route.trail_name
+    routeTitle.classList.add("result-heading")
     routeTitle.textContent = `${routeTitleName}`
-    resultCard.append(routeTitle)
+    resultRow1Title.append(routeTitle)
+
+    const resultRow1Distance = document.createElement("div")
+    resultRow1Distance.classList.add("result-row1-distance")
+    resultCardRow1.append(resultRow1Distance)
 
     const routeDistance = document.createElement("p")
     const routeDistanceName = route.trail_length
-    routeDistance.textContent = `${routeDistanceName}`
-    resultCard.append(routeDistance)
+    routeDistance.classList.add("result-distance")
+    routeDistance.textContent = `${routeDistanceName} miles`
+    resultRow1Distance.append(routeDistance)
+
+    const resultCardRow2 = document.createElement("div")
+    resultCardRow2.classList.add("result-row2")
+    resultCard.append(resultCardRow2)
 
     const routeSurface = document.createElement("p")
     const routeSurfaceName = route.surface_type
+    routeSurface.classList.add("result-surface")
     routeSurface.textContent = `${routeSurfaceName}`
-    resultCard.append(routeSurface)
+    resultCardRow2.append(routeSurface)
+
+
+    const resultCardRow3 = document.createElement("div")
+    resultCardRow3.classList.add("result-row3")
+    resultCard.append(resultCardRow3)
 
     const routeDescription = document.createElement("p")
     const routeDescriptionName = route.trail_description
+    routeDescription.classList.add("result-description")
     routeDescription.textContent = `${routeDescriptionName}`
-    resultCard.append(routeDescription)
+    resultCardRow3.append(routeDescription)
   })
 }
