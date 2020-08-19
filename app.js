ca3044b529b0e086e318432c875d536816f4a514
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-08-19 14:58:48 -0400

    got unit conversion working

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const resultsURL = (`https://data.ny.gov/resource/7bg2-3faq.json?$$app_token=rJK
 const descResults = (`https://data.ny.gov/resource/7bg2-3faq.json?$$app_token=rJKY8lYbv2sCllNIRE4Es2Lq4&$order=trail_length DESC&$where=`)
 const lengthURL = (`trail_length`)
 
-// const allResults = ("https://data.ny.gov/resource/7bg2-3faq.json?$$app_token=rJKY8lYbv2sCllNIRE4Es2Lq4");
-// const resultsURL = (`https://data.ny.gov/resource/7bg2-3faq.json?$$app_token=rJKY8lYbv2sCllNIRE4Es2Lq4&$order=trail_length&$where=`)
-// const lengthURL = (`trail_length`)
-
-
+// Add No Results Page - Currently not Working
 const noResultsPage = () => {
   const routeSection = document.querySelector(".results-section")
 
@@ -27,6 +23,7 @@ const noResultsPage = () => {
   noResults.append(noResultsLine2)
 }
 
+// Builds results to page in Miles
 const listRoutes = (routeData) => {
   routeData.forEach((route) => {
     const routeSection = document.querySelector(".results-section")
@@ -56,7 +53,7 @@ const listRoutes = (routeData) => {
     const routeDistance = document.createElement("p")
     const routeDistanceName = route.trail_length
     routeDistance.classList.add("result-distance")
-    routeDistance.textContent = `${routeDistanceName} miles`
+    routeDistance.textContent = `${routeDistanceName} mi`
     resultRow1Distance.append(routeDistance)
 
     const resultCardRow2 = document.createElement("div")
@@ -81,13 +78,87 @@ const listRoutes = (routeData) => {
   })
 }
 
+// Builds results to page in Kilos
+const listRoutesKm = (routeData) => {
+  routeData.forEach((route) => {
+    const routeSection = document.querySelector(".results-section")
+
+    const resultCard = document.createElement("div")
+    resultCard.classList.add("result-card")
+    routeSection.append(resultCard)
+
+    const resultCardRow1 = document.createElement("div")
+    resultCardRow1.classList.add("result-row1")
+    resultCard.append(resultCardRow1)
+
+    const resultRow1Title = document.createElement("div")
+    resultRow1Title.classList.add("result-row1-title")
+    resultCardRow1.append(resultRow1Title)
+
+    const routeTitle = document.createElement("h4")
+    const routeTitleName = route.trail_name
+    routeTitle.classList.add("result-heading")
+    routeTitle.textContent = `${routeTitleName}`
+    resultRow1Title.append(routeTitle)
+
+    const resultRow1Distance = document.createElement("div")
+    resultRow1Distance.classList.add("result-row1-distance")
+    resultCardRow1.append(resultRow1Distance)
+
+    // const routeDistance = document.createElement("p")
+    // const routeDistanceName = route.trail_length
+    // routeDistance.classList.add("result-distance")
+    // routeDistance.textContent = `${routeDistanceName} mi`
+    // resultRow1Distance.append(routeDistance)
+
+    const routeDistanceKM = document.createElement("p")
+    const routeDistanceLenKm = route.trail_length
+    let kmSum = routeDistanceLenKm * 1.60934
+    routeDistanceKM.classList.add("result-distance")
+    routeDistanceKM.textContent = `${kmSum} km`
+    resultRow1Distance.append(routeDistanceKM)
+
+    const resultCardRow2 = document.createElement("div")
+    resultCardRow2.classList.add("result-row2")
+    resultCard.append(resultCardRow2)
+
+    const routeSurface = document.createElement("p")
+    const routeSurfaceName = route.surface_type
+    routeSurface.classList.add("result-surface")
+    routeSurface.textContent = `${routeSurfaceName}`
+    resultCardRow2.append(routeSurface)
+
+    const resultCardRow3 = document.createElement("div")
+    resultCardRow3.classList.add("result-row3")
+    resultCard.append(resultCardRow3)
+
+    const routeDescription = document.createElement("p")
+    const routeDescriptionName = route.trail_description
+    routeDescription.classList.add("result-description")
+    routeDescription.textContent = `${routeDescriptionName}`
+    resultCardRow3.append(routeDescription)
+  })
+}
+
+const checkUnits = () => {
+  const unitsValue = document.querySelector("input[name=units-radios]:checked").value;
+  return unitsValue;
+}
+
 const fetchRoutes = async (url) => {
   try {
     const response = await axios.get(url)
     const routeData = response.data
+    const unitsURL = checkUnits();
 
-    listRoutes(routeData);
+    // listRoutes(routeData)
 
+    if (unitsURL === "kilos") {
+      listRoutesKm(routeData);
+    } else { 
+      listRoutes(routeData);
+    }
+    
     // if (routeData > 0) {
     //   listRoutes(routeData);
     // } else {
@@ -145,17 +216,18 @@ const checkSort = () => {
   return sortValue;
 }
 
-// const checkUnits = () => {
-//   const unitsValue = document.querySelector("input[name=units-radios]:checked").value;
-//   console.log(unitsValue)
-// }
-
 const removeResults = () => {
   const results = document.querySelector(".results-section")
   while (results.lastChild) {
     results.removeChild(results.lastChild)
   }
 } 
+
+const clearButton = document.querySelector(".clear-button")
+clearButton.addEventListener("click", () => {
+  removeResults();
+  fetchRoutes(allResults);
+})
 
 const runFilter = () => {
   
@@ -164,8 +236,7 @@ const runFilter = () => {
   const distanceURL = checkDistanceFilter();
   const surfaceURL = checkSurfaceFilter();
   const sortURL = checkSort();
-  // const unitsURL = checkUnits();
-
+      
   if (sortURL === "asc") {
     if (distanceURL === "all-distances" && surfaceURL !== "all-surfaces") { // If distance is all and surface has value 
       let url = (`${resultsURL}${surfaceURL}`)
@@ -203,53 +274,6 @@ filterButton.addEventListener("click", (e) => {
   runFilter();
 })
 
-// const filterButton = document.querySelector(".filter-button")
-// filterButton.addEventListener("click", (e) => {
-//   e.preventDefault()
-//   removeResults();
-  
-//   const distanceURL = checkDistanceFilter();
-//   const surfaceURL = checkSurfaceFilter();
-//   const sortURL = checkSort();
-//   // const unitsURL = checkUnits();
-
-//   if (sortURL === "asc") {
-//     if (distanceURL === "all-distances" && surfaceURL !== "all-surfaces") { // If distance is all and surface has value 
-//       let url = (`${resultsURL}${surfaceURL}`)
-//       fetchRoutes(url);
-//     } else if (distanceURL === "all-distances" && surfaceURL === "all-surfaces") { // If surface is all and distance is all 
-//       let url = allResults;
-//       fetchRoutes(url);
-//     } else if (surfaceURL === "all-surfaces" && distanceURL !== "all-distances") { // If surface is all and distance has value 
-//       let url = (`${resultsURL}${distanceURL}`)
-//       fetchRoutes(url);
-//     } else {
-//       let url = (`${resultsURL}${distanceURL} AND ${surfaceURL}`) // Distance and Surface has value
-//       fetchRoutes(url);
-//     }
-//   } else {
-//     if (distanceURL === "all-distances" && surfaceURL !== "all-surfaces") { // If distance is all and surface has value 
-//       let url = (`${descResults}${surfaceURL}`)
-//       fetchRoutes(url);
-//     } else if (distanceURL === "all-distances" && surfaceURL === "all-surfaces") { // If surface is all and distance is all 
-//       let url = allResultsDesc;
-//       fetchRoutes(url);
-//     } else if (surfaceURL === "all-surfaces" && distanceURL !== "all-distances") { // If surface is all and distance has value 
-//       let url = (`${descResults}${distanceURL}`)
-//       fetchRoutes(url);
-//     } else {
-//       let url = (`${descResults}${distanceURL} AND ${surfaceURL}`) // Distance and Surface has value
-//       fetchRoutes(url);
-//     }
-//   }
-// })
-
-const clearButton = document.querySelector(".clear-button")
-clearButton.addEventListener("click", () => {
-  removeResults();
-  fetchRoutes(allResults);
-})
-
 const toggleSortAsc = document.querySelector("#asc")
 toggleSortAsc.addEventListener("click", () => {
   runFilter();
@@ -260,30 +284,12 @@ toggleSortDesc.addEventListener("click", () => {
   runFilter();
 })
 
+const toggleMiles = document.querySelector("#miles")
+toggleMiles.addEventListener("click", () => {
+  runFilter();
+})
 
-
-// Working Code
-
-// const filterButton = document.querySelector(".filter-button")
-// filterButton.addEventListener("click", (e) => {
-//   e.preventDefault()
-
-//   removeResults();
-
-//   const distanceURL = checkDistanceFilter();  
-//   const surfaceURL = checkSurfaceFilter();
-
-//   if (distanceURL === "all-distances") { // If distance is all and surface has value 
-//     let url = (`${resultsURL}${surfaceURL}`)
-//     fetchRoutes(url);
-//   } else if (surfaceURL === "all-surfaces") { // If surface is all and surface has value 
-//     let url = (`${resultsURL}${distanceURL}`)
-//     fetchRoutes(url);
-//   } else if (distanceURL === "all-distances" || surfaceURL === "all-surfaces") {
-//     let url = allResults
-//     fetchRoutes(url);
-//   } else {
-//     let url = (`${resultsURL}${distanceURL} AND ${surfaceURL}`)
-//     fetchRoutes(url);
-//   }
-// })
+const toggleKilos = document.querySelector("#kilos")
+toggleKilos.addEventListener("click", () => {
+  runFilter();
+})
